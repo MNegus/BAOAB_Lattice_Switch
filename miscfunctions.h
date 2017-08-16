7f7bff7cabed7bf2cf9d8ab3afedeb03dd70d9fe
Michael Negus <michaelnegus1@gmail.com>
2017-08-16 16:50:40 +0100

    Added Box-Muller algorithm for normally distributing numbers

--- miscfunctions.h
+++ miscfunctions.h
@@ -4,6 +4,10 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
+#include "mt19937ar.h"
+
+#define PI 3.14159265358979323846264338327
 
 /* Function to use fscanf to read doubles from a line in an input file */
 void read_double(FILE *input_file, double *double_value) {
@@ -34,6 +38,13 @@ void read_int(FILE *input_file, int *int_value) {
     }
 }
 
+
+/* Returns a random normally distributed number, mean 0, standard deviation 1 */
+double box_muller_rand(){
+    double r1 = genrand_real3();
+    double r2 = genrand_real3();
+    return sqrt(-2 * log(r1)) * cos(2 * PI * r2);
+}
 
 
 #endif //BAOAB_LATTICE_SWITCH_MISCFUNCTIONS_H
