b9b57a7c5ae17d61ac7c44bfd6ee5add81f28b15
Michael Negus <michaelnegus1@gmail.com>
2017-08-16 18:02:09 +0100

    Added function for BAOAB_limit. Minimal documentation currently

--- main.c
+++ main.c
@@ -1,6 +1,25 @@
-#include "main.h"
 
-#include <stdio.h>
+#include "parameters.h"
+
+
+/* Implements a BAOAB step given an array of positions and forces acting on them */
+void BAOAB_limit(double *positions, double *forces, Parameters *params) {
+    // Creates new array of normally distributed values
+    double *new_normal_dist_arr = malloc(sizeof(double) * params->no_dimensions);
+    fill_arr_normal_dist(new_normal_dist_arr, params->no_dimensions);
+
+    for (long d = 0; d < params->no_dimensions; d++) {
+        // Updates all the positions using a BAOAB step
+        positions[d] += -params->timestep * forces[d] / params->mass + \
+        sqrt(0.5 * params->temp * params->timestep / params->mass) * (params->normal_dist_arr[d] + new_normal_dist_arr[d]);
+    }
+
+    // Swaps array pointers, so the array in the Parameters struct now points to the new array
+    swap_arr_pointers(&params->normal_dist_arr, &new_normal_dist_arr);
+    free(new_normal_dist_arr); // Frees the old array (now pointed to by new_normal_dist_arr)
+}
+
+
 
 int main(void) {
     return 0;
